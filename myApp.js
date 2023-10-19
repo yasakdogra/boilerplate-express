6cda1ced01f39be372be6ad6308c3e50bf19fd2b
Yasak Dogra <yasak.dogra@gmail.com>
2023-10-19 06:13:15 +0000

    add root level logger middleware

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ require("dotenv").config();
 
 console.log("Hello World");
 
+app.use(function (req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {

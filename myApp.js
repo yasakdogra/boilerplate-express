0b9605235ab4cbe2bb2fc8beec27eb18305656bb
Yasak Dogra <yasak.dogra@gmail.com>
2023-10-19 06:30:24 +0000

    add get route for name

--- myApp.js
+++ myApp.js
@@ -36,4 +36,11 @@ app.get("/:word/echo", function (req, res) {
   res.json({ echo: req.params.word });
 });
 
+app
+  .route("/name")
+  .get(function (req, res) {
+    res.json({ name: `${req.query.first} ${req.query.last}` });
+  })
+  .post(function (req, res) {});
+
 module.exports = app;

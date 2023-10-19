37f66f8bd03450cdc4633490c11bcbe93fd368e9
Yasak Dogra <yasak.dogra@gmail.com>
2023-10-19 05:47:15 +0000

    add json path

--- myApp.js
+++ myApp.js
@@ -3,10 +3,14 @@ let app = express();
 
 console.log("Hello World");
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
 });
 
 module.exports = app;
